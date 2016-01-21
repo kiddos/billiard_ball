c934d66c834eb09db83997d65ebbf76595eac8c2
kiddos <josephyu831007@yahoo.com.tw>
2016-01-21 23:21:32 +0800

    define common constants

---
+++ src/commons.c
@@ -0,0 +1,9 @@
+#include "commons.h"
+
+/* math constants */
+const double PI = 3.1415926535;
+
+/* window rendering constants */
+const double DEFAULT_WINDOW_WIDTH = 800;
+const double DEFAULT_WINDOW_HEIGHT = 600;
+const double FPS = 66.0;
