19fb49067676d20b25ae1d062763786a73101e11
kiddos <josephyu831007@yahoo.com.tw>
2016-01-21 23:59:20 +0800

    define function for warning and error messages

--- src/message.c
+++ src/message.c
@@ -5,3 +5,15 @@ void regular_message(const char* msg) {
   fprintf(stdout, "%s\n", msg);
 #endif
 }
+
+void warning_message(const char* msg) {
+#ifdef DEBUG
+  fprintf(stdout, "WARNING: %s\n", msg);
+#endif
+}
+
+void error_message(const char* msg) {
+#ifdef DEBUG
+  fprintf(stderr, "ERROR: %s\n", msg);
+#endif
+}
