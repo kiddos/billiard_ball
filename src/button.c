f3ca28ab4064ed3621ba0d31e7185a934e6d1355
kiddos <josephyu831007@yahoo.com.tw>
2016-01-22 20:32:39 +0800

    button init function

--- src/button.c
+++ src/button.c
@@ -1,12 +1,51 @@
 #include "button.h"
 
+#define TEXT_SIZE_SCALE 0.6
+
 button *button_init(rect r, const char *button_text,
                     const char *text_font_path,
                     const char *button_press_bitmap_path,
                     const char *button_release_bitmap_path,
                     void (*callback_function) (void)) {
+  const uint32_t text_size = r.height * TEXT_SIZE_SCALE;
   button *b = malloc(sizeof(button));
   // init with null;
+  b->pressed_bitmap = NULL;
+  b->release_bitmap = NULL;
+  b->text_font = NULL;
+
+  b->with_text = true;
+  b->is_pressed = false;
+  b->action_performed = false;
+
+  b->sx = r.x;
+  b->sy = r.y;
+  b->width = r.width;
+  b->height = r.height;
+
+  b->callback = callback_function;
+
+  // load bitmap and font
+  b->pressed_bitmap = al_load_bitmap(button_press_bitmap_path);
+  if (!b->pressed_bitmap) {
+    return NULL;
+  }
+
+  b->release_bitmap = al_load_bitmap(button_release_bitmap_path);
+  if (!b->release_bitmap) {
+    al_destroy_bitmap(b->pressed_bitmap);
+    return NULL;
+  }
+
+  b->text_font = al_load_font(text_font_path,
+                              text_size,
+                              ALLEGRO_TTF_NO_KERNING);
+  if (!b->text_font) {
+    al_destroy_bitmap(b->pressed_bitmap);
+    al_destroy_bitmap(b->release_bitmap);
+    return NULL;
+  }
+
   return b;
 }
 
@@ -15,7 +54,36 @@ button *button_init_without_text(rect r,
                                  const char *button_press_bitmap_path,
                                  const char *button_release_bitmap_path,
                                  void (*callback_function) (void)) {
+  button *b = malloc(sizeof(button));
+  // init with null;
+  b->pressed_bitmap = NULL;
+  b->release_bitmap = NULL;
+  b->text_font = NULL;
 
+  b->with_text = false;
+  b->is_pressed = false;
+  b->action_performed = false;
+
+  b->sx = r.x;
+  b->sy = r.y;
+  b->width = r.width;
+  b->height = r.height;
+
+  b->callback = callback_function;
+
+  // load bitmap and font
+  b->pressed_bitmap = al_load_bitmap(button_press_bitmap_path);
+  if (!b->pressed_bitmap) {
+    return NULL;
+  }
+
+  b->release_bitmap = al_load_bitmap(button_release_bitmap_path);
+  if (!b->release_bitmap) {
+    al_destroy_bitmap(b->pressed_bitmap);
+    return NULL;
+  }
+
+  return b;
 }
 
 void button_destroy(button *b) {
