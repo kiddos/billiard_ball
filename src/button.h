f9a7b2426e69e17e791fad7b10e1fe871c8ddd57
kiddos <josephyu831007@yahoo.com.tw>
2016-01-22 20:03:10 +0800

    button module

---
+++ src/button.h
@@ -0,0 +1,35 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "commons.h"
+
+typedef enum {
+  BUTTON_PRESS,
+  BUTTON_RELEASE
+} button_state, button_state_t;
+
+typedef struct button_t {
+  ALLEGRO_BITMAP *pressed_bitmap, *release_bitmap;
+  ALLEGRO_COLOR text_color;
+  bool with_text;
+  bool is_pressed;
+  bool action_performed;
+  double sx, sy, width, height;
+  void (*callback) (void);
+} button, button_t;
+
+button *button_init(rect r, const char *button_text,
+                    const char *button_press_bitmap_path,
+                    const char *button_release_bitmap_path,
+                    void (*callback_function) (void));
+button *button_init_without_text(rect r,
+                                 const char *button_press_bitmap_path,
+                                 const char *button_release_bitmap_path,
+                                 void (*callback_function) (void));
+void button_destroy(button *b);
+bool button_is_pressed(button* b);
+void button_update(button *b, uint64_t time_stamp);
+void button_draw(button *b);
+
+#endif /* end of include guard: BUTTON_H */
+
