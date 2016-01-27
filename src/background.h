05974008a6c69532828a3d09a719407b02cb4ad5
kiddos <josephyu831007@yahoo.com.tw>
2016-01-27 09:44:57 +0800

    clean up background module and add responsize function for resizing

--- src/background.h
+++ src/background.h
@@ -3,19 +3,20 @@
 
 #include "commons.h"
 
-extern const char* const BACKGROUND_CONFIG_FILE_PATH;
+typedef struct background_t {
+  ALLEGRO_BITMAP *table_bg;
+  ALLEGRO_BITMAP *vertical_border;
+  ALLEGRO_BITMAP *horizontal_border;
+  ALLEGRO_SAMPLE *background_music;
+  uint32_t bg_width, bg_height;
+  uint64_t last_update_time;
+} background, background_t;
 
-typedef struct background_t
-{
-	ALLEGRO_BITMAP *table_bg;
-	ALLEGRO_BITMAP *vertical_border;
-	ALLEGRO_BITMAP *horizontal_border;
-} background;
-
-
-background *create_background();
-void destroy_background(background *);
-void draw_background(background *);
+background *background_init(size window_size);
+void background_destroy(background *bg);
+void background_update(background *bg, const uint64_t time_stamp);
+void background_resize(background *bg, const size new_size);
+void background_draw(background *bg);
 
 #endif /* end of include guard: BACKGROUND_H */
 
