3f39390f7c3f81b3681a9a143bdd943948b14179
kiddos <josephyu831007@yahoo.com.tw>
2016-01-27 09:45:13 +0800

    table module

---
+++ src/table.h
@@ -0,0 +1,20 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "commons.h"
+
+extern const double TABLE_RATIO;
+extern const double TABLE_HOLE_RATIO;
+extern const double TABLE_BORDER_WIDTH_RATIO;
+
+typedef struct table_t {
+  ALLEGRO_BITMAP *bitmap;
+  double sx, sy, width, height;
+  double hole_radius, border_width;
+} table, table_t;
+
+table *table_init(const char* const table_texture, size window_size);
+void table_draw(table *t);
+void table_resize(table *t, size new_window_size);
+
+#endif /* end of include guard: TABLE_H */
